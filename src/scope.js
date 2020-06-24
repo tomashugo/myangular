e086b891ebf00b7c7dc7cad8020931d7ee432867
Tomas Hugo <tomas.hugo@gmail.com>
2020-06-24 06:16:43 -0300

    studying scope until page 175

--- src/scope.js
+++ src/scope.js
@@ -406,24 +406,44 @@ Scope.prototype.$on = function (eventName, listener) {
 };
 
 Scope.prototype.$emit = function (eventName) {
-  var event = { name: eventName };
+  var propagationStopped = false;
+  var event = {
+    name: eventName,
+    targetScope: this,
+    stopPropagation: function () {
+      propagationStopped = true;
+    },
+    preventDefault: function () {
+      event.defaultPrevented = true;
+    },
+  };
   var listenerArgs = [event].concat(_.tail(arguments));
 
   var scope = this;
   do {
+    event.currentScope = scope;
     scope.$$fireEventOnScope(eventName, listenerArgs);
     scope = scope.$parent;
-  } while (scope);
+  } while (scope && !propagationStopped);
+  event.currentScope = null;
   return event;
 };
 
 Scope.prototype.$broadcast = function (eventName) {
-  var event = { name: eventName };
+  var event = {
+    name: eventName,
+    targetScope: this,
+    preventDefault: function () {
+      event.defaultPrevented = true;
+    },
+  };
   var listenerArgs = [event].concat(_.tail(arguments));
   this.$$everyScope(function (scope) {
+    event.currentScope = scope;
     scope.$$fireEventOnScope(eventName, listenerArgs);
     return true;
   });
+  event.currentScope = null;
   return event;
 };
 
