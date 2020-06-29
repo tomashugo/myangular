7350bace2cec0bbac8a84ec79773518b65670e59
Tomas Hugo <tomas.hugo@gmail.com>
2020-06-28 21:47:13 -0300

    studying literal expressions until page 219

--- test/parse_spec.js
+++ test/parse_spec.js
@@ -55,17 +55,67 @@ describe("parse", function () {
 
   it("can parse a string in single quotes", function () {
     var fn = parse("'abc'");
-    expect(fn()).toEqual("abc");
+    expect(fn()).toEqual('abc');
   });
 
   it("can parse a string in double quotes", function () {
     var fn = parse('"abc"');
-    expect(fn()).toEqual("abc");
+    expect(fn()).toEqual('abc');
   });
 
   it("will not parse a string with mismatching quotes", function () {
     expect(function () {
-      parse("\"abc'");
+      parse("'a\\\'b'");
+      expect(fn()).toEqual('a\'b');
     }).toThrow();
   });
+
+  it("can parse a string with single quotes inside", function () {
+    var fn = parse('"a\\\"b"');
+    expect(fn()).toEqual('a\"b');
+  });
+
+  it("will parse a string with unicode escapes", function () {
+    var fn = parse('"\\u00A0"');
+    expect(fn()).toEqual('\u00A0');
+  });
+
+  it("will not parse a string with invalid unicode escapes", function () {
+    expect(function () { parse('"\\u00T0"'); }).toThrow();
+  });
+
+  it("will parse null", function () {
+    var fn = parse("null");
+    expect(fn()).toBe(null);
+  });
+
+  it("will parse true", function () {
+    var fn = parse("true");
+    expect(fn()).toBe(true);
+  });
+
+  it("will parse false", function () {
+    var fn = parse("false");
+    expect(fn()).toBe(false);
+  });
+
+  it("ignores whitespace", function () {
+    var fn = parse("\n42");
+    expect(fn()).toEqual(42);
+  });
+
+  it("will parse an empty array", function () {
+    var fn = parse('[]');
+    expect(fn()).toEqual([]);
+  });
+
+  it("will parse a non-empty array", function () {
+    var fn = parse('[1, "two", [3], true]');
+    expect(fn()).toEqual([1, 'two', [3], true]);
+  });
+
+  it("will parse an array with trailing commas", function () {
+    var fn = parse('[1, 2, 3, ]');
+    expect(fn()).toEqual([1, 2, 3])
+  })
 });
